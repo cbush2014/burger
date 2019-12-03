e22d55c9088e7b098dfe0b742541a42980ddfbc7
cbush19 <cbush5400@gmail.com>
2019-12-02 23:49:31 -0500

    need to revert back to previous version and fix and finalize homework

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,64 +1,20 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".btnDevour").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/quotes/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted id ", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
+  $(".btnDevour").on("click", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newQuote = {
-        author: $("#auth").val().trim(),
-        quote: $("#quo").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/quotes", {
-        type: "POST",
-        data: newQuote
-      }).then(
-        function() {
-          console.log("created new quote");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".update-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var updatedQuote = {
-        author: $("#auth").val().trim(),
-        quote: $("#quo").val().trim()
-      };
   
       var id = $(this).data("id");
   
       // Send the POST request.
-      $.ajax("/api/quotes/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: updatedQuote
+        data: 'scooby'
       }).then(
         function() {
-          console.log("updated quote");
+          console.log("updated devoured");
           // Reload the page to get the updated list
           location.assign("/");
         }
       );
     });
   });
-  
--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Set Handlebars as the default templating engine.
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -43,25 +38,58 @@ connection.connect( (err)=> {
 });
 
 // ROUTES
+
+app.get('/favicon.ico', (req, res) => {
+  res.end();
+});
+
 // Serve index.handlebars to the root route
 app.get('/', (req, res) => {
-    //handlebars requires an object be sent to the index.handlebars file
-    connection.query('SELECT * FROM burgers', (err, rows)=> {
+  //handlebars requires an object be sent to the index.handlebars file
+  connection.query('SELECT * FROM burgers', (err, rows)=> {
 
-  //  const leftSide = rows.filter(row => row.devored  );
-  //  const rightSide = rows.filter(row => !row.devored  );
-//  res.render('index', { animals: animals});
-  res.render('index', { burgers: rows});
-
-
-//        res.render("index", {burgers: rows});
-    });
-
+   res.render('index', { burgers: rows});
+  });
 });
- 
+
+// Post route -> back to home
+app.post("/", function(req, res) {
+  // Test it
+  console.log('You sent, ' + [req.body.newburger]);
+
+  // Test it
+  // return res.send('You sent, ' + req.body.task);
+
+  // When using the MySQL package, we'd use ?s in place of any values to be inserted,
+  // which are then swapped out with corresponding elements in the array
+  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
+  // https://en.wikipedia.org/wiki/SQL_injection
+  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", 
+  [req.body.newburger], function(err, result) {
+    if (err) throw err;
+
+    res.redirect("/");
+  });
+});
+
+// Update.  see public/assets/js and see what update code does (update-form) "PUT"
+app.put("/api/burgers/:id", function (req, res) {
+
+  const query =  connection.query('UPDATE burgers SET devoured = true WHERE ? ',
+  [ 
+       { id: req.params.id }
+  ], 
+  (err, result) => {
+    if (err) throw err;
+    res.end();
+  })
+  console.log(query.sql);
+});
+
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
